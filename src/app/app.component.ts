8fcd0720210bc351df5f10bbad2031d3f99cc1d0
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2020-05-23 20:35:23 -0300

    Hide header logged in information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from './services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ConectarSaludBackOffice';
+
+  loggedInRole = 'Administrativo';
 
   buttons = [
     {
@@ -17,5 +22,35 @@ export class AppComponent {
       title: 'CONSULTAS',
       href: 'medico/consultas'
     }
-  ]
+  ];
+
+  constructor(private _session: SessionService,
+              private _router: Router,) {
+  }
+
+  ngOnInit(){
+    const user = this._session.getUserFromSession();
+    if(user){
+      switch (user.role) {
+        case 'admin':
+          this.loggedInRole = 'Administrativo';
+          break;
+        case 'medic':
+          this.loggedInRole = 'Médico';
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  showLoggedInInformation(){
+    return this._session.getUserFromSession() !== null;
+  }
+
+  logout() {
+    this._session.removeUserFromSession();
+    this._router.navigate(['/login']);
+
+  }
 }

24665a9aaa173bbc2a0a9a34b13c56edfe0b0edd
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2020-07-14 19:57:47 -0300

    Added doctor functionality to dashboard

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ABMGenericFormField} from '../../models/generic-form-field';
 import {DoctorsService} from '../../services/doctors.service';
 import * as _ from 'lodash';
+import {SessionService} from "../../services/session.service";
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,6 @@ export class HomeComponent implements OnInit {
 
   ratingsTitleText = 'Calificaciones';
   consultationsTitleText = 'Consultas por especialidad';
-
 
   specialitiesData = {
     'total': 69,
@@ -51,11 +51,7 @@ export class HomeComponent implements OnInit {
   ratingsXAxisTitle = 'Día';
 
   ratingFilterData = [
-    new ABMGenericFormField({ name: 'doctor', value: '', title: 'Doctor', type: 'select', size: 'span-4', offsetRight: 'span-8',
-      lookups: [
-        { key: '1', value: 'Juan Perez' },
-        { key: '2', value: 'TODO doctor' },
-      ]}),
+    new ABMGenericFormField({ name: 'doctor', value: '', title: 'Doctor', type: 'select', size: 'span-4', offsetRight: 'span-8', lookups: []}),
     new ABMGenericFormField({ name: 'specialties', value: '', title: 'Especialidades', type: 'select', size: 'span-4', offsetRight: 'span-8',
       lookups: [
         { value: 'Inmunología', key: 'Inmunología' },
@@ -78,21 +74,30 @@ export class HomeComponent implements OnInit {
     new ABMGenericFormField({ name: 'date_begin', value: '', title: 'Fecha desde', type: 'date', size: 'span-5' }),
     new ABMGenericFormField({ name: 'date_end', value: '', title: 'Fecha hasta', type: 'date', size: 'span-4' }),
     new ABMGenericFormField({ name: 'submit', value: '', title: 'Filtrar', type: 'submit', size: 'span-1' }),
-
   ];
 
   consultationsFilterData = [];
 
-  constructor(private _doctorService: DoctorsService) {
+  private user: any;
+
+  constructor(private _doctorService: DoctorsService,
+              private _sessionService: SessionService) {
   }
 
   ngOnInit() {
+    this.user = this._sessionService.getUserFromSession();
+
     // initialize lookups
     this._doctorService.getAllDoctorsUsingGET({}).subscribe(data => {
       this.ratingFilterData[this.INDEX_DOCTOR_SELECT].lookups = _.map(data, (req) =>
         ({ key: req.id, value: req.first_name+' '+req.last_name })
       );
     });
+
+    if(this.isDoctorLoggedIn()){
+      this.ratingFilterData[this.INDEX_DOCTOR_SELECT].value = '71c4a137-601d-425d-8a95-7208b0c0ae73';
+      this.ratingFilterData[this.INDEX_DOCTOR_SELECT].disabled = true;
+    }
 
     // initialize second filter
     this.ratingFilterData.forEach(val => val.name != 'doctor'? this.consultationsFilterData.push(Object.assign({}, val)):[]);
@@ -119,9 +124,13 @@ export class HomeComponent implements OnInit {
 
   cleanForm(key){
     this[key] = this[key].map((field) => {
-      field.value = '';
+      field.value = (this.isDoctorLoggedIn() && field.name != 'doctor' || !this.isDoctorLoggedIn())?'':field.value;
       return field;
     });
+  }
+
+  isDoctorLoggedIn(){
+    return this.user.role !== 'admin';
   }
 
 }
